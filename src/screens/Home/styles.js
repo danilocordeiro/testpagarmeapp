85c846e32d26b8e403ba7e389ae66242bc431934
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-07-08 09:57:28 -0300

    Add Home

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+import { colors } from '../../utils/colors'
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: ${colors.white};
+  font-weight: bold;
+  align-self: center;
+  margin-vertical: 15px;
+`
